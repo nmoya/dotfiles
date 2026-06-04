4771dbc1f564ed5211b2590e9a629e5bf8bf0b2c
Nikolas Moya <nikolasmoya@gmail.com>
2026-06-04 21:47:18 +0200

    Add .gitconfig to the symlinks

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -5,3 +5,4 @@ ln -sfn $home_config_dir/.bashrc $HOME/.bashrc
 ln -sfn $home_config_dir/.inputrc $HOME/.inputrc
 ln -sfn $home_config_dir/.vimrc $HOME/.vimrc
 ln -sfn $home_config_dir/.zshrc $HOME/.zshrc
+ln -sfn $home_config_dir/.gitconfig $HOME/.gitconfig
