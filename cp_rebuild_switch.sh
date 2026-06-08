00275579845dd6f4851c99126da67c3887225bfe
Nikolas Moya <nikolasmoya@gmail.com>
2026-06-08 23:44:26 +0200

    Add application icons to waybar

--- cp_rebuild_switch.sh
+++ cp_rebuild_switch.sh
@@ -8,3 +8,6 @@ sway --unsupported-gpu --validate --config "$SWAY_CONFIG"
 
 sudo cp ./system/configuration.nix /etc/nixos/configuration.nix
 sudo nixos-rebuild switch
+
+GENERATION_NAME="$(basename "$(readlink /nix/var/nix/profiles/system)")"
+echo "Generation: $GENERATION_NAME"
